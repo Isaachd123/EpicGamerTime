f8d38b18ae8c0eb7a772440a943224ae33c4e0f6
Isaac <idowns.74728@farnborough.ac.uk>
2019-02-21 15:59:48 +0000

    Login path started

--- src/server/controllers/UserController.java
+++ src/server/controllers/UserController.java
@@ -1,4 +1,15 @@
 package server.controllers;
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("user/")
 public class UserController {
+    @POST
+    @Path("login")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String login(@FormParam(), @FormParam()) {
+        return;
+    }
 }
